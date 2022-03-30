ab0cd2988a46492d3a334c29e06d866b7180af54
fstipo <fstipanov@gmail.com>
2022-03-30 10:47:24 +0200

    first lesson

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const HelloWorld = () => {
-//   return <div>Hello World</div>;
-// };
-const Hello = () => {
-  return <span>Hello </span>;
-};
-
-const Franko = () => {
-  return <span>Franko</span>;
-};
-
-const HelloWorld = () => {
+const MyComponent = () => {
   return (
-    <>
-      <Hello />
-      <Franko />
-    </>
+    <div className="book">
+      <div className="title">The Title</div>
+      <div className="author">The Author</div>
+      <ul className="stats">
+        <li className="rating">5 stars</li>
+        <li className="isbn">12-345678-910</li>
+      </ul>
+      <div>Newline Test</div>
+      <div>Empty Newlines Here</div>
+      <div>&nbsp;Non-breaking &nbsp;Space&nbsp;</div>
+      <div>Line1 Line2</div>
+      <div>Line1 Line2</div>
+    </div>
   );
 };
 
-ReactDOM.render(<HelloWorld />, document.querySelector('#root'));
+ReactDOM.render(<MyComponent />, document.querySelector('#root'));

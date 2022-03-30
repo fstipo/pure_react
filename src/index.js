cd0088f3ab31fbd6658c1b178768a793c7f5b08c
fstipo <fstipanov@gmail.com>
2022-03-30 11:02:38 +0200

    Lesson 2: Components with React.createElement

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const MyComponent = () => {
-  return (
-    <div className="book">
-      <div className="title">The Title</div>
-      <div className="author">The Author</div>
-      <ul className="stats">
-        <li className="rating">5 stars</li>
-        <li className="isbn">12-345678-910</li>
-      </ul>
-      <div>Newline Test</div>
-      <div>Empty Newlines Here</div>
-      <div>&nbsp;Non-breaking &nbsp;Space&nbsp;</div>
-      <div>Line1 Line2</div>
-      <div>Line1 Line2</div>
-    </div>
+const MyComponent = (props) => {
+  return React.createElement(
+    'div',
+    { className: 'book' },
+    React.createElement('div', { className: 'title' }, 'The Title'),
+    React.createElement('div', { className: 'author' }, 'The Author'),
+    React.createElement(
+      'ul',
+      { className: 'stats' },
+
+      React.createElement('li', { className: 'rating' }, '5 stars'),
+      React.createElement('li', { className: 'isbn' }, '12-345678-910')
+    )
   );
+  //     <div className="book">
+  //       <div className="title">The Title</div>
+  //       <div className="author">The Author</div>
+  //       <ul className="stats">
+  //         <li className="rating">5 stars</li>
+  //         <li className="isbn">12-345678-910</li>
+  //       </ul>
+  //     </div>
 };
 
 ReactDOM.render(<MyComponent />, document.querySelector('#root'));

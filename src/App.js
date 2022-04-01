5f15a53e5d90eb502f35bb122667478521de10c2
fstipo <fstipanov@gmail.com>
2022-04-01 10:39:00 +0200

    style tweet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
 import Avatar from './Avatar';
+import Message from './Message';
+import Author from './Author';
+
+const Time = () => <span className="time">3h ago</span>;
+
+const ReplayButton = () => <i className="fa fa-reply reply-button" />;
+
+const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
+
+const LikeButton = () => <i className="fa fa-heart like-button" />;
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button" />
+);
 
 const App = () => {
   return (
     <div className="tweet">
       <Avatar />
-      Tweet
+      <div className="content">
+        <Author />
+        <Time />
+        <Message />
+
+        <div className="buttons">
+          <ReplayButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/Author.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Author = () => {
+  return (
+    <span className="author">
+      <span className="name">Franko</span>
+      <span className="handle">@Franko</span>
+    </span>
+  );
+};
+
+export default Author;

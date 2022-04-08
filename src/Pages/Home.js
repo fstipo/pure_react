5301cedf705c4675f3e1ace0f0bb59f994243750
fstipo <fstipanov@gmail.com>
2022-04-08 09:30:05 +0200

    add use state

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import About from './About';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
+const runInitial = () => {
+  console.log('We are the crow!');
+  return 4;
+};
 const Home = () => {
+  const [count, setCount] = useState(() => {
+    console.log('This is best way!');
+    return 10;
+  });
+  const decrementCount = () => setCount((prevCount) => prevCount - 1);
+  const incrementCount = () => setCount((prevCount) => prevCount + 1);
+
   return (
     <>
-      <main>
+      <main className="text-center py-5">
         <h2>Home page</h2>
         <p>You are the best</p>
+        <div className="display-4">
+          <button className="btn  btn-danger fw-bold" onClick={decrementCount}>
+            -
+          </button>
+          <span className="px-4">{count}</span>
+          <button className="btn btn-primary fw-bold" onClick={incrementCount}>
+            +
+          </button>
+        </div>
       </main>
       <nav>
         <Link to="/about">About</Link>
